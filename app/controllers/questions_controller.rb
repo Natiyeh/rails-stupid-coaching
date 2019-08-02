a7aadc861ed311676c7cc43d9691f72f7737796b
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-02 11:17:42 -0500

    fully updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,13 +3,22 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:question] == 'Tell me a joke.'
-      @response = ["I asked God for a bike, but I know God does't work that way so I stole a bike and asked for forgiveness.", "Throwing acid is wrong, in some people's eyes.", "My girlfriend told me to go out and get something that makes her look sexy... so I got drunk.", "Velcro - what a rip-off!", "I had a neck brace fitted years ago and I've never looked back since."].sample
+    if params[:question] == 'PG'
+      @response = ["I asked God for a bike, but I know God does't work that way so I stole a bike and asked for forgiveness.", "Throwing acid is wrong, in some people's eyes.", "Velcro - what a rip-off!", "I had a neck brace fitted years ago and I've never looked back since."].sample
+    elsif params[:question] == 'PG-13'
+      @response = ["What do you call a Mexican midget? ...A paragraph, because he's not a full essay.", "My girlfriend told me to go out and get something that makes her look sexy... so I got drunk."].sample
+    elsif params[:question] == 'R'
+      @response = ["Why does Santa Claus have such a big sack? ....He only comes once a year.", "Why does Dr. Pepper come in a bottle? ...Because his wife died."].sample
     elsif params[:question].include? '?'
       @response = ["Definitely", "Absolutely not", "Maybe", "Try again"].sample
     else
-      @response = ["EWWWWW!!!... but I said to ask me a question!!", "NOICE!!!... but I said to ask me a question!!!"].sample
+      @response = ["EWWWWW!!!... you don't pay attention!!", "NOICE!!!... but you did it wrong!!!"].sample
     end
+  end
+
+  def ask_question
+  end
+  def ask_joke
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'ask', to: 'questions#ask', as: :ask
   get 'answer', to: 'questions#answer', as: :answer
-  get 'ask_again', to: 'questions#ask_again', as: :ask_again
+  get 'ask_question', to: 'questions#ask_question', as: :ask_question
+  get 'ask_joke', to: 'questions#ask_joke', as: :ask_joke
 end
