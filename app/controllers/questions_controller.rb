d4acff56653c30b54296d2a9f47b3af5b1e48ef4
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-02 10:11:23 -0500

    made it more

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,12 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:question] == 'I am going to work'
-      @response = 'Great!'
+    if params[:question] == 'Tell me a joke.'
+      @response = ["I asked God for a bike, but I know God does't work that way so I stole a bike and asked for forgiveness.", "Throwing acid is wrong, in some people's eyes.", "My girlfriend told me to go out and get something that makes her look sexy... so I got drunk.", "Velcro - what a rip-off!", "I had a neck brace fitted years ago and I've never looked back since."].sample
     elsif params[:question].include? '?'
-      @response = 'Silly question, get dressed and go to work!'
+      @response = ["Definitely", "Absolutely not", "Maybe", "Try again"].sample
     else
-      @response = "I don't care, get dressed and go to work!"
+      @response = ["EWWWWW!!!... but I said to ask me a question!!", "NOICE!!!... but I said to ask me a question!!!"].sample
     end
   end
 end
